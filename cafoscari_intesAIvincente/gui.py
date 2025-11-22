b40cac80fa213deeece9536f447de37d017a43b7
Matteo Brini <brinimatteo@gmail.com>
2025-11-22 13:45:11 +0100

    added fsm, added styles, added first layouts

--- cafoscari_intesAIvincente/gui.py
+++ cafoscari_intesAIvincente/gui.py
@@ -1,17 +1,19 @@
-from PySide6.QtWidgets import QMainWindow, QLabel
+from PySide6.QtWidgets import QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget
 from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtCore import Qt
+
+from cafoscari_intesAIvincente.fsm import StateMachine
 
 import rc_images
 import rc_icons
 
 class MainWindow(QMainWindow):
 
-    __MAIN_WINDOW_TITLE = "CaFoscari IntesAI Vincente"
-    __MAIN_WINDOW_WIDTH = 1280
-    __MAIN_WINDOW_HEIGHT = 720
-    __MAIN_WINDOW_WIDTH_MIN = 640
-    __MAIN_WINDOW_HEIGHT_MIN = 360
+    __MAIN_WINDOW_TITLE         = "CaFoscari IntesAI Vincente"
+    __MAIN_WINDOW_WIDTH         = 1280
+    __MAIN_WINDOW_HEIGHT        = 720
+    __MAIN_WINDOW_WIDTH_MIN     = 640
+    __MAIN_WINDOW_HEIGHT_MIN    = 360
 
     __ASPECT_RATIO = __MAIN_WINDOW_WIDTH / __MAIN_WINDOW_HEIGHT
 
@@ -27,10 +29,41 @@ class MainWindow(QMainWindow):
         self._background_label.setAlignment(Qt.AlignCenter)
 
         self._background_pixmap = QPixmap(":/images/background.png")
-        self.update_background()
 
         self._icon_pixmap = QPixmap(":/icons/icon.ico")
         self.setWindowIcon(QIcon(self._icon_pixmap))
+
+        self._btn_start_game = QPushButton("Start Game", self)
+        self._btn_start_game.setMinimumSize(200, 50)
+        
+        self._btn_settings = QPushButton("Settings", self)
+        self._btn_settings.setMinimumSize(200, 50)
+        
+        self._btn_exit = QPushButton("Exit", self)
+        self._btn_exit.setMinimumSize(200, 50)
+        self._btn_exit.clicked.connect(self.close)
+
+        self._btn_back_from_settings = QPushButton("Back", self)
+        self._btn_back_from_settings.setMinimumSize(200, 50)
+        
+        self._btn_back_from_gameplay = QPushButton("Back to Menu", self)
+        self._btn_back_from_gameplay.setMinimumSize(200, 50)
+
+        self._hide_all_buttons()
+
+        self._fsm = StateMachine(self)
+        self._fsm.state_main_menu.entered.connect(self.on_main_menu_entered)
+        self._fsm.state_settings.entered.connect(self.on_settings_entered)
+        self._fsm.state_gameplay.entered.connect(self.on_gameplay_entered)
+        
+        self._btn_start_game.clicked.connect(self._fsm.go_to_gameplay.emit)
+        self._btn_settings.clicked.connect(self._fsm.go_to_settings.emit)
+        self._btn_back_from_settings.clicked.connect(self._fsm.go_to_main_menu.emit)
+        self._btn_back_from_gameplay.clicked.connect(self._fsm.go_to_main_menu.emit)
+        
+        self.update_background()
+        
+        self._fsm.start()
 
     def resizeEvent(self, event):
         super().resizeEvent(event)
@@ -52,3 +85,49 @@ class MainWindow(QMainWindow):
             Qt.SmoothTransformation
         )
         self._background_label.setPixmap(scaled_pixmap)
+        
+        self._position_buttons()
+    
+    def _hide_all_buttons(self):
+        self._btn_start_game.hide()
+        self._btn_settings.hide()
+        self._btn_exit.hide()
+        self._btn_back_from_settings.hide()
+        self._btn_back_from_gameplay.hide()
+    
+    def _position_buttons(self):
+        btn_width = 200
+        btn_height = 50
+        
+        center_x = (self.width() - btn_width) // 2
+        
+        start_y = int(self.height() * 0.55)
+        
+        spacing = int(self.height() * 0.1)
+        
+        self._btn_start_game.move(center_x, start_y)
+        self._btn_settings.move(center_x, start_y + spacing)
+        self._btn_exit.move(center_x, start_y + spacing * 2)
+        
+        back_margin = 20
+        back_x = back_margin
+        back_y = self.height() - btn_height - back_margin
+        self._btn_back_from_settings.move(back_x, back_y)
+        self._btn_back_from_gameplay.move(back_x, back_y)
+
+    def on_main_menu_entered(self):
+        self._hide_all_buttons()
+        self._btn_start_game.show()
+        self._btn_settings.show()
+        self._btn_exit.show()
+        self._position_buttons()
+
+    def on_settings_entered(self):
+        self._hide_all_buttons()
+        self._btn_back_from_settings.show()
+        self._position_buttons()
+
+    def on_gameplay_entered(self):
+        self._hide_all_buttons()
+        self._btn_back_from_gameplay.show()
+        self._position_buttons()
